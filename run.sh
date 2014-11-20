b27975c5b166f93484735b0311eccb20bd609f8b
Yuriy Arhipov <yuriyarhipovua@yandex.ru>
2014-11-20 22:24:38 +0300

    [#56] columns sorted

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export C_FORCE_ROOT="true"
 service nginx restart &
 service postgresql restart &
-service rabbitmq-server restart &
+service rabbitmq-server restart
 gunicorn_django --bind localhost:8001 &
 python manage.py celeryd -l info &
