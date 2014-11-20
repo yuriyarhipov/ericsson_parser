3a866125be0bc316f13f8674a9e01ccb26ac192d
Yuriy Arhipov <yuriyarhipovua@yandex.ru>
2014-11-20 22:17:28 +0300

    [#56] columns sorted

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-service nginx restart
-service postgresql restart
-service rabbitmq-server restart
+service nginx restart &
+service postgresql restart &
+service rabbitmq-server restart &
 gunicorn_django --bind xml.yura.cc:8001 &
 python manage.py celeryd -l info &
