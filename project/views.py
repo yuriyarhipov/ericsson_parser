bd667828f020aa566e3c369acf6b5528e73b0584
Yuriy Arhipov <yuriyarhipovua@yandex.ru>
2014-11-24 22:01:38 +0300

    [#62]  tree

--- project/views.py
+++ project/views.py
@@ -29,9 +29,9 @@ def save_project(request):
 def treeview(request, project):
     project = Project.objects.get(project_name=project)
     data = [{'id': 'network', 'label': 'Network Configuration', 'children': [
-        {'id': '2g', 'label': '2G', 'children': project.get_tree('2g', 'txt')},
-        {'id': '3g', 'label': '3G', 'children': project.get_tree('3g', 'xml')},
-        {'id': '4g', 'label': '4G', 'children': project.get_tree('4g', 'xml')}
+        {'id': '2g', 'label': 'GSM', 'children': [{'id': '2g_ERICSSON', 'label': 'ERICSSON', 'children': project.get_tree('2g', 'txt')}]},
+        {'id': '3g', 'label': 'WCDMA', 'children': [{'id': '3g_ERICSSON', 'label': 'ERICSSON', 'children': project.get_tree('3g', 'xml')}]},
+        {'id': '4g', 'label': 'LTE', 'children': [{'id': '4g_ERICSSON', 'label': 'ERICSSON', 'children': project.get_tree('4g', 'xml')}]}
     ]}, ]
 
     return HttpResponse(json.dumps(data), content_type='application/json')
