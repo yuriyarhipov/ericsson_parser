e9cd6bfeaa4a961270ca22c116d8a2a368411c5b
Yuriy Arhipov <yuriyarhipovua@yandex.ru>
2015-08-17 11:45:41 +0300

    audit templates

---
+++ frontend_xml/app/js/controllers/audit.js
@@ -0,0 +1,6 @@
+var auditControllers = angular.module('auditControllers', []);
+
+auditControllers.controller('setAuditTemplateCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        console.log('OK');
+  }]);
