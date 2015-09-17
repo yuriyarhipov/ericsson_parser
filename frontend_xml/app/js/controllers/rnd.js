37029cfc97377edd1892333a14e3cdfd96f0c99b
Yuriy Arhipov <yuriyarhipovua@yandex.ru>
2015-09-17 16:04:27 +0300

    rnd navigation

--- frontend_xml/app/js/controllers/rnd.js
+++ frontend_xml/app/js/controllers/rnd.js
@@ -22,46 +22,41 @@ rndControllers.controller('rndCtrl', ['$scope', '$http', '$routeParams',
 
   }]);
 
-rndControllers.controller('mapCtrl', ['$scope', '$http', '$routeParams',
-    function ($scope, $http, $routeParams) {
+rndControllers.controller('mapCtrl', ['$scope', '$http', '$routeParams', '$location', 'leafletData',
+    function ($scope, $http, $routeParams, $location, leafletData) {
+        var search_params = $location.search();
         $scope.default_center = {};
-        $scope.layers = {
-                    baselayers: {
-                        mapbox_light: {
-                            name: 'Mapbox Light',
-                            url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-                            type: 'xyz',
-                            layerOptions: {
-                                apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
-                                mapid: 'bufanuvols.lia22g09'
-                            }
-                        },
-                        osm: {
-                            name: 'OpenStreetMap',
-                            url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                            type: 'xyz'
-                        }
-                    }
-                }
         var rnd_network = $routeParams.network;
 
         var markerIcon = {
             iconUrl: 'static/sector_blue.png',
         }
 
+        var get_icon = function(columns, point, params){
+            var icon = {
+                iconUrl: 'static/sector_blue.png'
+            }
+            for (var i=0;i<columns.length;i+=1){
+                if (params[columns[i]]) {
+                    if (point[columns[i]] == params[columns[i]]){
+                        icon.iconUrl ='static/sector_red.png';
+                        break;
+                    }
+                }
+            }
+            return icon
+        };
 
         $http.get('/data/rnd/' + rnd_network + '/').success(function(data) {
             var markers = {};
-            var data = data.data;
             var columns = data.columns;
-
-
+            var data = data.data;
 
             for(i=0;i<data.length;i+=1){
                 markers[data[i].Utrancell] = {
                     'lat': data[i].Latitud,
                     'lng': data[i].Longitud,
-                    'icon': markerIcon,
+                    'icon': get_icon(columns, data[i], search_params),
                     'iconAngle': data[i].Azimuth,
                     message: "Utrancell:" + data[i].Utrancell,
                 }
@@ -72,7 +67,14 @@ rndControllers.controller('mapCtrl', ['$scope', '$http', '$routeParams',
             $scope.default_center.lat = data[0].Latitud;
             $scope.default_center.lng = data[0].Longitud;
             $scope.default_center.zoom = 10;
-
-
+            //leafletData.getMap().then(function(map) {
+            //    marker = new L.circle([51.5, -0.09], 500, {
+            //        startAngle: 45,
+            //        stopAngle: 135,
+            //
+            //         });
+            //    marker.addTo(map);
+            //    map.setView([51.505, -0.09], 13);
+            //});
         });
   }]);
